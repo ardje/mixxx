49ea7943d7fe3ba92109523cdb478a2ea6b84998
ard <ard@kwaak.net>
2019-11-15 12:15:51 +0000

    Reflect what has been changed in the .cpp

--- src/effects/builtin/overdriveeffect.h
+++ src/effects/builtin/overdriveeffect.h
@@ -13,14 +13,14 @@
 struct OverdriveGroupState : public EffectState {
     // Default accumulator to 1 so we immediately pick an input value.
     OverdriveGroupState(const mixxx::EngineParameters& bufferParameters)
-            : EffectState(bufferParameters),
+            : EffectState(bufferParameters) /*,
               hold_l(0),
               hold_r(0),
-              accumulator(1) {
+              accumulator(1)*/ {
     }
-    CSAMPLE hold_l, hold_r;
+    //CSAMPLE hold_l, hold_r;
     // Accumulated fractions of a samplerate period.
-    CSAMPLE accumulator;
+    //CSAMPLE accumulator;
 };
 
 class OverdriveEffect : public EffectProcessorImpl<OverdriveGroupState> {
@@ -44,8 +44,7 @@ class OverdriveEffect : public EffectProcessorImpl<OverdriveGroupState> {
         return getId();
     }
 
-    EngineEffectParameter* m_pBitDepthParameter;
-    EngineEffectParameter* m_pDownsampleParameter;
+    EngineEffectParameter* m_pGainParameter;
 
     DISALLOW_COPY_AND_ASSIGN(OverdriveEffect);
 };
